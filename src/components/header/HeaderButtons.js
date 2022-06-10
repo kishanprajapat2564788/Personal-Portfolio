7435dc4817071c76aabb242a258051051aba2099
Kishan Prajapat <47397967+kishanprajapat2564788@users.noreply.github.com>
2022-06-10 10:49:45 +0530

    Update HeaderButtons.js

--- src/components/header/HeaderButtons.js
+++ src/components/header/HeaderButtons.js
@@ -5,7 +5,7 @@ const HeaderButtons = () => {
   return (
     <div className='headerButtons'>
         <a href={CV} download className='btn'>Download CV</a>
-        <a href='#conatct' className='btn btn-primary'>Let's Talk</a>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
